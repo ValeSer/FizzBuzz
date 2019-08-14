5706ce2c636ab93f388c4720e610f7f17f5bb8f4
mackacavs <mackenziegdixon@yahoo.com>
2019-08-14 11:46:35 +0100

    Latest changes

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,10 @@ describe 'fizzbuzz' do
   it 'expects fizz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'expects buzz' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'expects fizzbuzz' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
